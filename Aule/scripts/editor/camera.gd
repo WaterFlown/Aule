4f49d79cd1ef7dfd141a222c13de34466eca916c
WaterFlow <kin.nemescivolvap@gmail.com>
2025-12-05 19:39:07 +0100

    Placeholder changes and camera zooming fixes
    Camera now zooms towards mouse

--- Aule/scripts/editor/camera.gd
+++ Aule/scripts/editor/camera.gd
@@ -2,11 +2,14 @@ class_name EditorCamera extends Camera2D
 
 var target_zoom = zoom
 @export var zoom_increment = Vector2(0.5,0.5)
-@export var zoom_speed = 12.0
+@export var zoom_speed = 24.0
 @export var max_zoom = Vector2(1.5,1.5)
 @export var min_zoom = Vector2(0.7,0.7)
 
 @export var xy_scroll_limits = Vector2(1600,1600)
+
+var mouse_pos := get_global_mouse_position()
+var new_mouse_pos := get_global_mouse_position()
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -27,7 +30,17 @@ func _input(event):
 					set_physics_process(true)
 
 func _physics_process(delta):
-	if is_equal_approx(zoom.x, target_zoom.x):
+	if custom_approx_equal(zoom.x, target_zoom.x) :
+		zoom = target_zoom
 		set_physics_process(false)
 	else:
+		mouse_pos = get_global_mouse_position()
 		zoom = lerp(zoom, target_zoom, zoom_speed*delta)
+		new_mouse_pos = get_global_mouse_position()
+		position += mouse_pos - new_mouse_pos
+		
+
+func custom_approx_equal(a: float, b:float):
+	if (a == b):
+		return true;
+	return abs(a - b) < 0.002;
